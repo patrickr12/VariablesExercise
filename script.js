7d9a7a875243b243d478039a10ef3656be615949
Patrick Rutazuyaza <rutazuyazapatrick@gmail.com>
2022-10-04 21:48:45 -0500

    VariablesExercise

---
+++ script.js
@@ -0,0 +1,49 @@
+// 1
+// const
+
+// 2
+let num = 20;
+
+// 3
+num = 15;
+
+// 4
+num += 7;
+// num = num + 7;
+// console.log(num);
+
+// 5
+num--;
+// console.log(num);
+
+// 6
+num = "This is not a number";
+// console.log(num);
+
+// 7
+let blank;
+
+// 8
+console.log(blank);
+
+// 9
+blank = null;
+console.log(blank);
+
+// BONUS
+// 1
+const quarter = 0.25, dime = 0.1, nickel = 0.05,  penny = 0.01;
+
+// const [quarter, dime, nickel, penny] = [0.25, 0.10, 0.05, 0.01];
+
+// 2
+console.log(quarter, dime, nickel, penny);
+
+// 3
+let letters = "dfrtuvhidp";
+
+// 4
+console.log(letters[letters.length - 1]);
+
+// console.log(letters.length);
+// console.log(letters[9]);
